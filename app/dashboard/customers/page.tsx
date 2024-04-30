1450e99291f742da46fb10d98dd7dfdd5973dca3
liuyilong <redtea25@qq.com>
2024-04-30 19:00:27 +0800

    客户

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,25 @@
 import { Metadata } from 'next';
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const customers = fetchFilteredCustomers(query);
+
+  return (
+    <div>
+      <CustomersTable customers={[]} />
+    </div>
+  );
 }
